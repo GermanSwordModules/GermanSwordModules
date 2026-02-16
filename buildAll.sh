f49c83248884b425ed00ac1a3fe378dc53b66580
Jens <>
2026-02-16 13:27:05 +0100

    Fixed Script

--- buildAll.sh
+++ buildAll.sh
@@ -8,11 +8,13 @@ mkdir -p build/alpha/sword/packages
 mkdir -p build/beta/sword/modules/genbook/rawgenbook
 mkdir -p build/beta/sword/mods.d
 mkdir -p build/beta/sword/packages
+mkdir -p build/alpha/sword/modules/comments/zcom
+mkdir -p build/alpha/sword/modules/texts/ztext
 
 home=$(pwd)
 shopt -s globstar
 
-# build alpha genbooks
+## build alpha genbooks
 for i in alpha/genbook/**/*.conf; do # Whitespace-safe and recursive
 	base_name=$(basename -s .conf ${i})
 	xml_file=${i%.conf}.xml
@@ -40,34 +42,36 @@ for i in alpha/comments/**/*.conf; do # Whitespace-safe and recursive
 	base_name=$(basename -s .conf ${i})
 	xml_file=${i%.conf}.xml
     echo "Processing $base_name"
-    mkdir -p build/alpha/sword/comments/zcom/$base_name
+    mkdir -p build/alpha/sword/modules/comments/zcom/$base_name
     cp $i build/alpha/sword/mods.d
     # osis2mod
-    cd build/alpha/sword/comments/zcom/$base_name
-    timeout 120 osis2mod . - < ../../../../../../$xml_file   -v German
+    cd build/alpha/sword/modules/comments/zcom/$base_name
+    timeout 10 osis2mod . - < ../../../../../../../$xml_file   -v German
     cd -
     # copy figures 
-    cp alpha/comments/$base_name/*.png build/alpha/sword/comments/zcom/$base_name/
-    cp alpha/comments/$base_name/*.jpg build/alpha/sword/comments/zcom/$base_name/
-    cp alpha/comments/$base_name/*.jpeg build/alpha/sword/comments/zcom/$base_name/
+    cp alpha/comments/$base_name/*.png build/alpha/sword/modules/comments/zcom/$base_name/
+    cp alpha/comments/$base_name/*.jpg build/alpha/sword/modules/comments/zcom/$base_name/
+    cp alpha/comments/$base_name/*.jpeg build/alpha/sword/modules/comments/zcom/$base_name/
     # make zip genbook
     cd build/alpha/sword/
-    zip -r $base_name.zip mods.d/$base_name.conf comments/zcom/$base_name/*
+    zip -r ${base_name}.zip mods.d/$base_name.conf modules/comments/zcom/$base_name/*
     cd -
     # move zip file
     mv build/alpha/sword/$base_name.zip build/alpha/sword/packages/
 done
 
- build alpha bible texts
+cd $home 
+
+# build alpha bible texts
 for i in alpha/texts/**/*.conf; do # Whitespace-safe and recursive
 	base_name=$(basename -s .conf ${i})
 	xml_file=${i%.conf}.xml
     echo "Processing $base_name"
-    mkdir -p build/alpha/sword/texts/ztext/$base_name
+    mkdir -p build/alpha/sword/modules/texts/ztext/$base_name
     cp $i build/alpha/sword/mods.d
     # osis2mod
-    cd build/alpha/sword/texts/ztext/$base_name
-    timeout 120 osis2mod . - < ../../../../../../$xml_file   -v German
+    cd build/alpha/sword/modules/texts/ztext/$base_name
+    timeout 10 osis2mod . - < ../../../../../../../$xml_file   -v German >/dev/null
     cd -
     # copy figures 
     cp alpha/texts/$base_name/*.png build/alpha/sword/texts/ztext/$base_name/
@@ -75,13 +79,13 @@ for i in alpha/texts/**/*.conf; do # Whitespace-safe and recursive
     cp alpha/texts/$base_name/*.jpeg build/alpha/sword/texts/ztext/$base_name/
     # make zip genbook
     cd build/alpha/sword/
-    zip -r $base_name.zip mods.d/$base_name.conf texts/ztext/$base_name/*
+    zip -r ${base_name}.zip mods.d/$base_name.conf modules/texts/ztext/$base_name/*
     cd -
     # move zip file
     mv build/alpha/sword/$base_name.zip build/alpha/sword/packages/
 done
 
-# build alpha lexdict
+## build alpha lexdict
 for i in alpha/lexdict/**/*.conf; do # Whitespace-safe and recursive
 	base_name=$(basename -s .conf ${i})
 	xml_file=${i%.conf}.xml
@@ -111,9 +115,9 @@ cd -
 cp -r build/alpha/sword/packages $REPOFOLDER/alpha/
 cp -r build/alpha/sword/mods.d $REPOFOLDER/alpha/
 cp  build/alpha/sword/mods.d.tar.gz $REPOFOLDER/alpha/
-cp  -r build/alpha/sword/modules $REPOFOLDER/alpha/
+#cp  -r build/alpha/sword/modules $REPOFOLDER/alpha/
 
-# build beta genbooks
+## build beta genbooks
 for i in beta/genbook/**/*.conf; do # Whitespace-safe and recursive
 	base_name=$(basename -s .conf ${i})
 	xml_file=${i%.conf}.xml
@@ -139,8 +143,9 @@ done
 cd build/beta/sword/
 tar -czf mods.d.tar.gz mods.d/*
 cd -
+cd $home
 # copy to repo
 cp -r build/beta/sword/packages $REPOFOLDER/beta/
 cp -r build/beta/sword/mods.d $REPOFOLDER/beta/
 cp  build/beta/sword/mods.d.tar.gz $REPOFOLDER/beta/
-cp  -r build/beta/sword/modules $REPOFOLDER/beta/
+#cp  -r build/beta/sword/modules $REPOFOLDER/beta/
